a022a0cd7b6cac0fa5548f2c7de345374b6d43ba
Emil.B <emil@enliven.co>
2019-03-12 22:50:59 +0100

    added react router

---
+++ src/Teams.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Route, Link} from "react-router-dom";
+class Teams extends React.Component {
+  render() {
+    return <h1>Teams!</h1>
+  }
+}
+export default Teams;
