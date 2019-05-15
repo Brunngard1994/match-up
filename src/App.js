c0626331d308bf3e4999f79f4a275de2d075d301
Emil.B <emil@enliven.co>
2019-05-15 04:01:37 +0200

    added new navbar and created signup/login with firebase backend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
+import fire from './config/Fire';
 import Navbar from './components/Navbar'
 import Header from './Header'
 import Intro from './Intro'
-import Register from './Register'
+import Login from './Login'
+import Home from './Home'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = ({
+      user: null,
+    });
+    this.authListener = this.authListener.bind(this);
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      if (user) {
+        this.setState({ user });
+        localStorage.setItem('user', user.uid);
+      } else {
+        this.setState({ user: null });
+        localStorage.removeItem('user');
+      }
+    });
+  }
+
   render() {
     return (
     <Router>
@@ -16,10 +44,9 @@ class App extends Component {
           <Route path="/" exact component={Header} />
           <Route path="/Header" component={Header} />
           <Route path="/Intro" component={Intro} />
-          <Route path="/Register" component={Register} />
+          {this.state.user ? (<Home />) : (<Login />)}
         </Switch>
-       
-      </div>
+       </div>
     </Router>
     );
 
@@ -27,10 +54,6 @@ class App extends Component {
 
 }
 
-const Home = () => (
-  <div>
-    <h1>Home</h1>
-  </div>
-);
+
 
 export default App; 
